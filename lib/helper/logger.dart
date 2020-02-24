394dd6f663b67034cc1ddf447624122d92727e81
Alann Maulana <alann_it06@yahoo.co.id>
2020-02-24 16:36:54 +0700

    add flag logger, add tag for logging

--- lib/helper/logger.dart
+++ lib/helper/logger.dart
@@ -1,4 +1,12 @@
-void logPrintWrapped(String text) {
-  final pattern = new RegExp('.{1,800}'); // 800 is the size of each chunk
+bool logEnabled = false;
+
+void logPrintWrapped(String text, {String tag = 'AAD'}) {
+  if (!logEnabled) return;
+
+  if (tag != null) {
+    text = '$tag: $text';
+  }
+
+  final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
   pattern.allMatches(text).forEach((match) => print(match.group(0)));
 }
