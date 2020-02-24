5106109373729e6fefb6498720f90f6dd0db6a13
Alann Maulana <alann_it06@yahoo.co.id>
2020-02-24 16:31:08 +0700

    formatting and add logger

--- lib/request_token.dart
+++ lib/request_token.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:aad_oauth/helper/logger.dart';
 import 'package:http/http.dart';
 
 import 'model/config.dart';
@@ -17,22 +18,33 @@ class RequestToken {
 
   Future<Token> requestToken(String code) async {
     _generateTokenRequest(code);
+    logPrintWrapped(code, tag: 'TOKEN-CODE');
     return await _sendTokenRequest(
-        _tokenRequest.url, _tokenRequest.params, _tokenRequest.headers);
+      _tokenRequest.url,
+      _tokenRequest.params,
+      _tokenRequest.headers,
+    );
   }
 
   Future<Token> requestRefreshToken(String refreshToken) async {
     _generateTokenRefreshRequest(refreshToken);
-    return await _sendTokenRequest(_tokenRefreshRequest.url,
-        _tokenRefreshRequest.params, _tokenRefreshRequest.headers);
+    logPrintWrapped(refreshToken, tag: 'TOKEN-REFRESH');
+    return await _sendTokenRequest(
+      _tokenRefreshRequest.url,
+      _tokenRefreshRequest.params,
+      _tokenRefreshRequest.headers,
+    );
   }
 
-  Future<Token> _sendTokenRequest(String url, Map<String, String> params,
-      Map<String, String> headers) async {
+  Future<Token> _sendTokenRequest(
+    String url,
+    Map<String, String> params,
+    Map<String, String> headers,
+  ) async {
     Response response = await post(url, body: params, headers: headers);
+    logPrintWrapped(response.body, tag: 'TOKEN-REQUEST');
     Map<String, dynamic> tokenJson = json.decode(response.body);
-    Token token = Token.fromJson(tokenJson);
-    return token;
+    return Token.fromJson(tokenJson);
   }
 
   void _generateTokenRequest(String code) {
